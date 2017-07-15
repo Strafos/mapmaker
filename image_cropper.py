fe7a3bb883b2f8b79a39dbda40889f3003ab4366
Zaibo Wang <zaibowang@gmail.com>
2017-07-15 02:25:55 -0400

    Split cropping and aspect ratio fitting to separate programs

--- image_cropper.py
+++ image_cropper.py
@@ -1,26 +1,14 @@
 from PIL import Image
 import math
 
-img = Image.open('FULL_MAP.jpg')
-
-ratio = input('Enter desired aspect ratio (ex. 4:3): \n')
-idx = ratio.find(':')
-height = float(ratio[0:idx])
-width = float(ratio[idx + 1: ])
-
-if height > width:
-    width = width / height
-    height = 1.0
-else:
-    height = height / width
-    width = 1.0
+img = Image.open('croppedMap.8.jpg')
 
 img_width, img_height = img.size
 half_width = img_width / 2
 half_height = img_height / 2
 
-new_width = img_width * width
-new_height = img_height * height
+new_width = img_width * .6
+new_height = img_height * .6
 
 img = img.crop(
     (
